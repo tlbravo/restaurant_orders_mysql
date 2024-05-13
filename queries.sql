6751bfb34216ba1c581637ed552fbf18f2b3d892
tlbravo <tbravo507@gmail.com>
2024-05-13 19:32:29 -0400

    updates

--- queries.sql
+++ queries.sql
@@ -79,3 +79,33 @@ FROM(
 	HAVING COUNT(item_id) > 12) AS num_orders_over_twelve;
 --20 orders with more than 12 items
 
+--Combine the menu_items and order_details tables into a single table
+SELECT *
+FROM order_details AS od
+LEFT JOIN menu_items AS mi
+	ON od.item_id = mi.menu_item_id;
+
+--What were the least and most ordered items? What categories were they in?
+SELECT item_name, category, COUNT (order_details_id) AS num_purchases
+FROM order_details AS od
+LEFT JOIN menu_items AS mi
+	ON od.item_id = mi.menu_item_id
+GROUP BY item_name, category
+ORDER BY num_purchases DESC;
+
+--What were the top 5 orders that spent the most money?
+SELECT order_id, SUM(price) AS total_spent
+FROM order_details AS od
+LEFT JOIN menu_items AS mi
+	ON od.item_id = mi.menu_item_id
+GROUP BY order_id
+ORDER BY total_spent DESC
+LIMIT 5;
+
+--highest spent order
+SELECT category, COUNT(item_id) AS num_items
+FROM order_details AS od
+LEFT JOIN menu_items AS mi
+	ON od.item_id = mi.menu_item_id
+WHERE order_id = 440
+GROUP BY category;
